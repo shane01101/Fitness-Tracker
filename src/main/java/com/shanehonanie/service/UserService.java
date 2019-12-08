5538b23bf4991f6bf1511aa7f269f19655c060c9
Shane Honanie <shaneh425@gmail.com>
2019-12-07 18:14:51 -0800

    Spring Security and Create User Endpoint Public

--- src/main/java/com/shanehonanie/service/UserService.java
+++ src/main/java/com/shanehonanie/service/UserService.java
@@ -1,7 +1,8 @@
 package com.shanehonanie.service;
 
 import com.shanehonanie.shared.dto.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto user);
 }
--- src/main/java/com/shanehonanie/service/impl/UserServiceImpl.java
+++ src/main/java/com/shanehonanie/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.shanehonanie.shared.Utils;
 import com.shanehonanie.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -41,5 +43,11 @@ public class UserServiceImpl implements UserService {
         BeanUtils.copyProperties(storedUserDetails, returnValue);
 
         return returnValue;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        // TODO:loadUserByUsername
+        return null;
     }
 }
